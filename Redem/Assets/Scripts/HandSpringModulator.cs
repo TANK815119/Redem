7fb528d91b39e3f2c16f7dda848f79530ab6895d
TANK815119 <reid.jacobsen@comcast.net>
2024-03-09 19:13:50 -0600

    More Accurate Physics IK

--- Redem/Assets/Scripts/HandSpringModulator.cs
+++ Redem/Assets/Scripts/HandSpringModulator.cs
@@ -6,24 +6,73 @@ public class HandSpringModulator : MonoBehaviour
 {
     [SerializeField] private Transform hand;
     [SerializeField] private Transform controller;
+    [SerializeField] private Transform shoulder;
     [SerializeField] private float springMax = 99999f;
     [SerializeField] private float springMin = 500f;
     [SerializeField] private float angularSpringMax = 9999f;
     [SerializeField] private float angularSpringMin = 50f;
     [SerializeField] private float modulatorMult = 2f;
+    [SerializeField] private bool useShoulderDistance = true;
 
     private ConfigurableJoint joint;
     private Vector3 offset;
+    private float maxArmDistance;
     // Start is called before the first frame update
     void Start()
     {
         joint = hand.gameObject.GetComponent<ConfigurableJoint>();
         joint.rotationDriveMode = RotationDriveMode.Slerp; //force slerp
         offset = joint.connectedAnchor;
+        maxArmDistance = Vector3.Distance(controller.position, shoulder.position);
     }
 
     // Update is called once per frame
     void FixedUpdate()
+    {
+       if(useShoulderDistance)
+       {
+            ShoulderControllerModulator();
+       }
+       else
+       {
+            HandCOntrollerModulator();
+       }
+    }
+
+    private void ShoulderControllerModulator()
+    {
+        //fetch the drives
+        JointDrive drive = joint.xDrive;
+        JointDrive angularDrive = joint.slerpDrive;
+
+        float relativeDistance = Vector3.Distance(controller.position, shoulder.position) - maxArmDistance;
+        if(relativeDistance < 0)
+        {
+            relativeDistance = 0f;
+        }
+        else
+        {
+            Debug.Log("modulating");
+        }
+
+        float modulator = 2f * modulatorMult * (float)System.Math.Tanh(relativeDistance);
+        drive.positionSpring = springMax - springMax * modulator;
+        angularDrive.positionSpring = angularSpringMax - angularSpringMax * modulator;
+
+        if (drive.positionSpring < springMin)
+        {
+            drive.positionSpring = springMin;
+            angularDrive.positionSpring = angularSpringMin;
+        }
+
+        //apply to the joint
+        joint.xDrive = drive;
+        joint.yDrive = drive;
+        joint.zDrive = drive;
+        joint.slerpDrive = angularDrive;
+    }
+
+    private void HandCOntrollerModulator()
     {
         //fetch the drives
         JointDrive drive = joint.xDrive;
@@ -34,7 +83,7 @@ public class HandSpringModulator : MonoBehaviour
         drive.positionSpring = springMax - springMax * modulator;
         angularDrive.positionSpring = angularSpringMax - angularSpringMax * modulator;
 
-        if(drive.positionSpring < springMin)
+        if (drive.positionSpring < springMin)
         {
             drive.positionSpring = springMin;
             angularDrive.positionSpring = angularSpringMin;
