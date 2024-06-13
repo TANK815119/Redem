5821c1a4c665652fa388206cc62e14f9f4eee5e9
TANK815119 <reid.jacobsen@comcast.net>
2024-06-13 17:37:00 -0500

    grab-priority for ownership
    The transitions are still jittery, but--because the grabber of the object is preferred--the object no long "oscillates" between owners. The physics syncing is much cleaner, but I think it could do with an ownership transitioner that does something like--instead of taking the rigidbody data at time of transition in the new owner's perspective--wait a bit to account for delay in the simulations+some sort of smooth transition.

--- Redem/Assets/NetworkOwnershipAura.cs
+++ Redem/Assets/NetworkOwnershipAura.cs
@@ -3,27 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 
-public class NetworkOwnershipAura : MonoBehaviour
+public class NetworkOwnershipAura : NetworkBehaviour
 {
     [SerializeField] private NetworkObject networkObject;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.TryGetComponent<NetworkObject>(out NetworkObject otherNetObject) && NetworkManager.Singleton.IsHost)
+        if (other.attachedRigidbody.gameObject.TryGetComponent<NetworkObject>(out NetworkObject otherNetObject) && NetworkManager.Singleton.IsHost)
         {
             //avoid ownership conflicts
             NetworkObject otherPlayerNetObject = NetworkManager.Singleton.ConnectedClients[otherNetObject.OwnerClientId].PlayerObject;
             if (!otherPlayerNetObject.OwnerClientId.Equals(networkObject.OwnerClientId))
             {
+                //make sure the object isnt grabbed
+                if (GrabbedByOtherPlayer(other.attachedRigidbody.transform))
+                {
+                    return; //dont try to change ownership
+                }
+
+                //collect all the auras of the other player
                 List<NetworkOwnershipAura>  auraList = new List<NetworkOwnershipAura>();
                 FindAuras(otherPlayerNetObject.transform, auraList);
 
                 //check if this NetworkOwnershipAura is closest, and assign if is
                 bool closestAura = true;
-                float thisAuraDist = Vector3.Distance(this.transform.position, other.transform.position);
+                float thisAuraDist = Vector3.Distance(this.transform.position, other.attachedRigidbody.transform.position);
                 for (int i = 0; i < auraList.Count; i++)
                 {
-                    float outherAuraDist = Vector3.Distance(auraList[i].transform.position, other.transform.position);
+                    float outherAuraDist = Vector3.Distance(auraList[i].transform.position, other.attachedRigidbody.transform.position);
                     if(outherAuraDist < thisAuraDist)
                     {
                         closestAura = false;
@@ -40,6 +47,63 @@ public class NetworkOwnershipAura : MonoBehaviour
         }
     }
 
+    //alternative to this method, I could look for all the joints connected to any given component of the gameobject
+    //and then check if it is either tagged or layered as the body
+    //then I can get acces to the players who grabbed the object
+    //but that appears not to be neccessay right now
+    //private bool FindIfGrabbed(Transform parent) //checks if the object is grabbed
+    //{
+    //    for (int i = 0; i < parent.childCount; i++)
+    //    {
+    //        if(FindIfGrabbed(parent.GetChild(i)))
+    //        {
+    //            return true;
+    //        }
+    //    }
+
+    //    if (parent.gameObject.TryGetComponent(out GrabPoint grabPoint))
+    //    {
+    //        if (grabPoint.Grabbed)
+    //        {
+    //            return true;
+    //        }
+    //        else
+    //        {
+    //            return false;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        return false;
+    //    }
+    //}
+    private bool GrabbedByOtherPlayer(Transform parent) //checks if the object is grabbed
+    {
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            if (GrabbedByOtherPlayer(parent.GetChild(i)))
+            {
+                return true;
+            }
+        }
+
+        if (parent.gameObject.TryGetComponent(out ConfigurableJoint joint))
+        {
+            Debug.Log("has attached joint");
+            NetworkObject jointedNetObject = GetParentNetworkObject(joint.connectedBody.gameObject.transform);
+            if (jointedNetObject != null)
+            {
+                Debug.Log("has network");
+                if (!jointedNetObject.OwnerClientId.Equals(networkObject.OwnerClientId))
+                {
+                    Debug.Log("heterogenous");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     private void FindAuras(Transform parent, List<NetworkOwnershipAura> auraList)
     {
         if (parent.gameObject.TryGetComponent(out NetworkOwnershipAura parentNetObject))
@@ -51,5 +115,22 @@ public class NetworkOwnershipAura : MonoBehaviour
         {
             FindAuras(parent.GetChild(i), auraList);
         }
+    }
+
+    private NetworkObject GetParentNetworkObject(Transform childTransform)
+    {
+        Transform currentTransform = childTransform;
+
+        while (currentTransform != null)
+        {
+            if (currentTransform.gameObject.TryGetComponent(out NetworkObject netObject))
+            {
+                return netObject;
+            }
+
+            currentTransform = currentTransform.parent;
+        }
+
+        return null; // No NetworkObject found in the parent hierarchy
     }
 }
