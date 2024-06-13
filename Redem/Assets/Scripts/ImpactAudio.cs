1049ffdc596499111489df4289397afbca0e1986
TANK815119 <reid.jacobsen@comcast.net>
2024-06-13 18:11:38 -0500

    Impact sound cooldown
    default is 0.25 seconds

--- Redem/Assets/Scripts/ImpactAudio.cs
+++ Redem/Assets/Scripts/ImpactAudio.cs
@@ -8,16 +8,27 @@ public class ImpactAudio : MonoBehaviour
     [SerializeField] private AudioClip audioClip;
     [SerializeField] [Range(0.0f, 2.0f)] private float volume = 1.0f;
     [SerializeField] private float forceMinimum = 2f;
+    [SerializeField] private float cooldown = 0.25f;
 
     private Rigidbody body;
+    private float cooldownState = 0f;
 
     private void Start()
     {
         body = gameObject.GetComponent<Rigidbody>();
     }
+
+    private void Update()
+    {
+        if(cooldownState >= 0f)
+        {
+            cooldownState -= Time.deltaTime;
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.relativeVelocity.magnitude > forceMinimum)
+        if(collision.relativeVelocity.magnitude > forceMinimum && cooldownState <= 0f)
         {
             //modulate audio based on relative force
             float collisionVolume = 1f;
@@ -31,6 +42,8 @@ public class ImpactAudio : MonoBehaviour
                 collisionVolume = Mathf.Pow(collision.relativeVelocity.magnitude / 10f, 2f);
             }
             AudioSource.PlayClipAtPoint(audioClip, collision.contacts[0].point, collisionVolume * volume * 0.5f);
+
+            cooldownState = cooldown;
         }
     }
 }
