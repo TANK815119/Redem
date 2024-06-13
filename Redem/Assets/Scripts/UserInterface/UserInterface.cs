cca1f9a6f0a1e576cf1301b678d94e5089658ed8
TANK815119 <reid.jacobsen@comcast.net>
2024-06-13 15:35:12 -0500

    Removed Bodily Layers
    The intra-body interactions are not scripted

--- Redem/Assets/Scripts/UserInterface/UserInterface.cs
+++ Redem/Assets/Scripts/UserInterface/UserInterface.cs
@@ -29,7 +29,7 @@ public class UserInterface : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         inputData.rightController.TryGetFeatureValue(CommonUsages.secondaryButton, out bool menuPressed);
 
